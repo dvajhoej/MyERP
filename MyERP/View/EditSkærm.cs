915a6dee1179866aa9da408281bddf63228912bc
Nortorus <mads.sp@hotmail.com>
2024-09-02 12:43:37 +0200

    #V3

---
+++ MyERP/View/EditSkærm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHCOOL.UI;
+
+namespace MyERP.View
+{
+    public class EditSkærm(Virksomhed virksomhed) : Screen
+    {
+        public override string Title { get; set; } = "Rediger Virksomhed";
+
+        protected override void Draw()
+        {
+            Clear();
+
+            Form<Virksomhed> editor = new Form<Virksomhed>();
+
+            editor.TextBox("Firmanavn", "Navn");
+            editor.TextBox("Vej", "Vej");
+            editor.TextBox("Husnummer", "Husnummer");
+            editor.TextBox("Postnummer", "Postnummer");
+            editor.TextBox("By", "By");
+            editor.TextBox("Land", "Land");
+            editor.TextBox("Valuta", "Valuta");
+
+            editor.Edit(virksomhed);
+
+            Clear();
+            Console.WriteLine($"Virksomhed {virksomhed.Navn} er blevet opdateret.");
+        }
+    }
+}
