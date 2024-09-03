c9c8d5295eaa933aa9cdbf543ec77f85a0893338
Nortorus <mads.sp@hotmail.com>
2024-09-03 12:12:55 +0200

    #D6

---
+++ MyERP/Kunde.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyERP
+{
+    public class Kunde : Person
+    {
+        public int Kundenummer { get; set; }
+        public DateTime SidsteKøbDato { get; set; }
+
+        public Kunde(string fornavn, string efternavn, Adresse adresse, string email, string telefon, int kundenummer, DateTime sidsteKøbDato)
+            : base(fornavn, efternavn, adresse, email, telefon)
+        {
+            Kundenummer = kundenummer;
+            SidsteKøbDato = sidsteKøbDato;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Kundenummer: {Kundenummer}, Sidste køb: {SidsteKøbDato.ToShortDateString()}";
+        }
+    }
+}
