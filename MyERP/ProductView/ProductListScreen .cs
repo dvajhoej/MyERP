132bb175b3cdd61ac5332df58d1429308c8f3d49
Dvajhoej <Dvajhoej@gmail.com>
2024-09-09 11:20:27 +0200

    #P5/6/7

--- MyERP/ProductView/ProductListScreen .cs
+++ MyERP/ProductView/ProductListScreen .cs
@@ -1,5 +1,6 @@
 ﻿using TECHCOOL.UI;
 using System;
+using MyERP.productView;
 
 namespace MyERP.ProductView
 {
@@ -11,10 +12,10 @@ namespace MyERP.ProductView
         {
             listPage = new ListPage<Product>();
 
-            listPage.Add(new Product { ProductNumber = 01, Name = "Reb", Description = "Et langt reb", SellingPrice = 100.00m, PurchasePrice = 99.00m, Location = "223A", QuantityInStock = 55, Unit = UnitType.Meter });
-            listPage.Add(new Product { ProductNumber = 02, Name = "Dæk", Description = "Et lækkert dæk", SellingPrice = 100.00m, PurchasePrice = 30.00m, Location = "283E", QuantityInStock = 10, Unit = UnitType.Styk });
-            listPage.Add(new Product { ProductNumber = 03, Name = "Dukke", Description = "En dukke for piger", SellingPrice = 100.00m, PurchasePrice = 80.00m, Location = "648N", QuantityInStock = 20, Unit = UnitType.Styk });
-            listPage.Add(new Product { ProductNumber = 04, Name = "Gummiand", Description = "Den bedste ting i verden", SellingPrice = 200.00m, PurchasePrice = 175.00m, Location = "42AA", QuantityInStock = 1, Unit = UnitType.Styk });
+            listPage.Add(new Product { ProductNumber = 01, Name = "Reb", Description = "Et langt reb", SellingPrice = 100.00, PurchasePrice = 99.00, Location = "223A", QuantityInStock = 55, Unit = UnitType.Meter });
+            listPage.Add(new Product { ProductNumber = 02, Name = "Dæk", Description = "Et lækkert dæk", SellingPrice = 100.00, PurchasePrice = 30.00, Location = "283E", QuantityInStock = 10, Unit = UnitType.Styk });
+            listPage.Add(new Product { ProductNumber = 03, Name = "Dukke", Description = "En dukke for piger", SellingPrice = 100.00, PurchasePrice = 80.00, Location = "648N", QuantityInStock = 20, Unit = UnitType.Styk });
+            listPage.Add(new Product { ProductNumber = 04, Name = "Gummiand", Description = "Den bedste ting i verden", SellingPrice = 200.00, PurchasePrice = 175.00, Location = "42AA", QuantityInStock = 1, Unit = UnitType.Styk });
         }
 
         public override string Title { get; set; } = "Produkter";
@@ -27,13 +28,14 @@ namespace MyERP.ProductView
             Console.WriteLine("Tryk F2 for at tilpasse et produkt");
             Console.WriteLine("Tryk F5 for at slette et produkt");
 
-            //listPage.AddKey(ConsoleKey.F1, CreateProduct);
-            //listPage.AddKey(ConsoleKey.F2, EditProduct);
+            listPage.AddKey(ConsoleKey.F1, CreateProduct);
+            listPage.AddKey(ConsoleKey.F2, EditProduct);
             listPage.AddKey(ConsoleKey.F5, DeleteProduct);
 
             listPage.AddColumn("Varenummer", "ProductNumber");
             listPage.AddColumn("Navn", "Name");
             listPage.AddColumn("Antal På Lager", "QuantityInStock");
+            listPage.AddColumn("Enhed", "Unit");
             listPage.AddColumn("Indkøbspris", "PurchasePrice");
             listPage.AddColumn("Salgspris", "SellingPrice");
             listPage.AddColumn("Avance (%)", "MarginPercentage");
@@ -50,17 +52,17 @@ namespace MyERP.ProductView
             }
         }
 
-        //private void CreateProduct(Product product)
-        //{
-        //    var newProduct = new Product();
-        //    listPage.Add(newProduct);
-        //    Screen.Display(new ProductCreateScreen(newProduct));
-        //}
+        private void CreateProduct(Product product)
+        {
+            var newProduct = new Product();
+            listPage.Add(newProduct);
+            Screen.Display(new ProductCreateScreen(newProduct));
+        }
 
-        //private void EditProduct(Product selected)
-        //{
-        //    Screen.Display(new ProductEditScreen(selected));
-        //}
+        private void EditProduct(Product selected)
+        {
+            Screen.Display(new ProductEditScreen(selected));
+        }
 
         private void DeleteProduct(Product selected)
         {
