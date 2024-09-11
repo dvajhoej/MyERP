de72458294f2d9a35716a2067d5863b71ba350c6
Dvajhoej <Dvajhoej@gmail.com>
2024-09-11 13:29:39 +0200

    DbHelper

---
+++ MyERP/Database/DbHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyERP.DBHELPER
+{
+    public static class DatabaseString
+    {
+        public const string ConnectionString = "Data Source=h1sql.cloudprog.org;User ID=H1PD070124_Gruppe1;Password=H1PD070124_Gruppe1;Initial Catalog=H1PD070124_Gruppe1;";
+    }
+}
+
+
