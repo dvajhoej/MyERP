25434283157bbdcf64d3df4fc5fba0906e829226
Dvajhoej <Dvajhoej@gmail.com>
2024-09-18 14:01:10 +0200

    #S: Minor update, TotalPrice added to sale, updated query

--- MyERP/Database/CustomerDatabase.cs
+++ MyERP/Database/CustomerDatabase.cs
@@ -66,7 +66,7 @@ namespace MyERP
                             Country = reader.GetString(7),
                             Phone = reader.GetString(8),
                             Email = reader.GetString(9),
-                            LastPurchaseDate = reader.GetDateTime(10),
+                            LastPurchaseDate = reader.IsDBNull(10) ? (DateTime?)null : reader.GetDateTime(10),
                             AddressID = reader.GetInt32(11),
                             PersonID = reader.GetInt32(12)
                         };
