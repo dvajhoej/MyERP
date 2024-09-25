64c9600c23cdce7bdce2e3661ae86ba4e185b17c
Nortorus <mads.sp@hotmail.com>
2024-09-25 08:55:14 +0200

    #S4 Edit screen

--- MyERP/SalesView/SalesListScreen.cs
+++ MyERP/SalesView/SalesListScreen.cs
@@ -64,7 +64,7 @@ namespace MyERP.SalesView
         }
         private void EditOrder(SalesOrderHeader selected)
         {
-            //Screen.Display(new OrdertEditScreen(selected));
+            Screen.Display(new SalesEditScreen(selected));
         }
 
         private void DeleteOrder(SalesOrderHeader selected)
