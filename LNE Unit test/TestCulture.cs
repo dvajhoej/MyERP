3e819b27363f11111eb4a60b7ca140c624d063c0
Dvajhoej <Dvajhoej@gmail.com>
2024-10-03 17:59:06 +0200

    Test, culture

---
+++ LNE Unit test/TestCulture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xunit;
+
+namespace LNE_Unit_test
+{
+    public class TestCulture
+    {
+        [Fact]
+        public void EnsureCultureIsDanish()
+        {
+            var culture = new CultureInfo("da-DK");
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+
+            // Act
+            var currentCulture = CultureInfo.CurrentCulture;
+
+            Assert.Equal("da-DK", currentCulture.Name);
+        }
+    }
+}
