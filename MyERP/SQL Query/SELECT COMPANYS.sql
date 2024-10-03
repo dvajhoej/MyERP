b120b3373209aecb3ea96993777677df56596cf5
Dvajhoej <Dvajhoej@gmail.com>
2024-10-03 13:44:19 +0200

    update

--- MyERP/SQL Query/SELECT COMPANYS.sql
+++ MyERP/SQL Query/SELECT COMPANYS.sql
@@ -1,3 +1,10 @@
-SELECT        Companies.name, Addresses.street, Addresses.houseNumber, Addresses.zipCode, Addresses.city, Addresses.country, companies.currency
-FROM            Addresses INNER JOIN
-                         companies ON Addresses.addressID = companies.addressID
+SELECT      
+			Companies.name, 
+			Addresses.street,
+			Addresses.houseNumber,
+			Addresses.zipCode,
+			Addresses.city,
+			Addresses.country,
+			companies.currency
+FROM Addresses 
+full JOIN companies ON Addresses.addressID = companies.addressID
